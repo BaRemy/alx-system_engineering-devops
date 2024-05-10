f1108bee62dd207fde1fc74029a56a8f8cc800a6
BaRemy <bahatiremy05@gmail.com>
2024-05-10 00:56:29 -0700

    Update: Fix Apache 500 Error & Automate with Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
-file { '/var/www/html/wp-settings.php':
-  ensure => present,
-  replace => {
-    '.phpp' => '.php',
-  }
+$file_to_edit = '/var/www/html/wp-settings.php'
+
+exec { 'replace_line':
+  command => "sed -i 's/phpp/php/g' ${file_to_edit}",
+  path    => ['/bin','/usr/bin']
 }
